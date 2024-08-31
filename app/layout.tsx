7ecbaf7103819bcb78b0945bbdb646e197a96c2d
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-08-30 22:36:24 -0700

    Delete unused font and rename title layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
-  title: "Website Template",
-  description: "This is a website template",
+  title: "Website Title",
+  description: "This is a website description",
 };
 
 export default function RootLayout({
@@ -16,7 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="relative">{children}</body>
     </html>
   );
 }
