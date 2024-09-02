8b62380f65fab8201e760d6df5aa41d2ebb71709
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-02 14:19:13 -0700

    Create private top bar component.

---
+++ components/private/TopBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopBar = () => {
+    return (
+        <div className="px-4 py-2 border-b border-gray-200 shadow-md">
+            <h1>Dashboard</h1>
+        </div>
+    );
+};
+
+export default TopBar;
--- components/private/index.ts
+++ components/private/index.ts
@@ -1,5 +1,7 @@
 import LeftPanel from "@/components/private/LeftPanel";
+import TopBar from "@/components/private/TopBar";
 
 export { 
-    LeftPanel
+    LeftPanel,
+    TopBar
 };
