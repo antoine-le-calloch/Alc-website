eafa7d230dd89fa73839681a9adea0301b556071
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-02 11:25:20 -0700

    Correct bad use of font size.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,27 +5,27 @@ const Footer = () => {
         <div className="bg-black text-white">
             <div className="container">
                 <div className="flex items-center justify-center flex-wrap px-4 py-8 gap-4">
-                    <a href="/about" className="text-l font-bold uppercase">
+                    <a href="/about" className="font-bold uppercase">
                         About
                     </a>
                     |
-                    <a href="/contact" className="text-l font-bold uppercase">
+                    <a href="/contact" className="font-bold uppercase">
                         Contact
                     </a>
                     |
-                    <a href="/privacy_policy" className="text-l font-bold uppercase">
+                    <a href="/privacy_policy" className="font-bold uppercase">
                         Privacy Policy
                     </a>
                     |
-                    <a href="/refund_policy" className="text-l font-bold uppercase">
+                    <a href="/refund_policy" className="font-bold uppercase">
                         Refund Policy
                     </a>
                     |
-                    <a href="/shipping_policy" className="text-l font-bold uppercase">
+                    <a href="/shipping_policy" className="font-bold uppercase">
                         Shipping Policy
                     </a>
                     |
-                    <a href="/terms_of_sale" className="text-l font-bold uppercase">
+                    <a href="/terms_of_sale" className="font-bold uppercase">
                         Terms of Sale
                     </a>
                 </div>
