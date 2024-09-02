e08d4262a726dad21f206282e58a7483c40ca306
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-02 14:25:03 -0700

    move the code from dashboard page to layout.

--- app/(private)/admin/dashboard/page.tsx
+++ app/(private)/admin/dashboard/page.tsx
@@ -1,25 +1,12 @@
 "use client";
 
 import React from 'react';
-import Image from "next/image";
 
 const DashboardPage: React.FC = () => {
     return (
-        <div className="container max-w-full">
-            <div className="flex">
-                <div className="h-screen bg-amber-100 p-4">
-                    <Image src="/logo.svg" alt="Logo" width={40} height={40} />
-                </div>
-                <div className="flex flex-col w-full">
-                    <div className="p-4 border-b border-gray-200">
-                        <h1>Dashboard</h1>
-                    </div>
-                    <div className="p-4">
-                        <div>
-                            content
-                        </div>
-                    </div>
-                </div>
+        <div className="container p-4">
+            <div>
+                Content loading...
             </div>
         </div>
     )
