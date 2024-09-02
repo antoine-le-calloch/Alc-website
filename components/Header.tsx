891b4abe84ef144cf5187bc60aa00cbfe2eedb18
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-01 21:12:08 -0700

    change header border color.

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Header = () => {
     return (
-        <div className="border-b border-black">
+        <div className="border-b border-gray-200">
             <div className="container mx-auto">
                 <div className="flex items-center justify-between p-4">
                     <div className="flex items-center">
