a5bb567c5ecbf353ea4f786e40e6c75f4735bbc3
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-01 21:10:38 -0700

    Add container to encompass header section and increase link text size.

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,23 +6,29 @@ import Image from "next/image";
 const Header = () => {
     return (
         <div className="border-b border-black">
-            <div className="flex items-center justify-between p-4">
-                <div className="flex items-center">
-                    <a href="/">
-                        <Image src="/logo.png" alt="Logo" width={60} height={60}/>
-                    </a>
-                </div>
-                <div className="flex items-center gap-x-4">
-                    <a href="/about">About</a>
-                    <a href="/contact">Contact</a>
-                </div>
-                <div className="flex items-center gap-x-4">
-                    <a href="/account">
-                        <UserCircleIcon className="size-6"/>
-                    </a>
-                    <a href="/cart">
-                        <ShoppingCartIcon className="size-6"/>
-                    </a>
+            <div className="container mx-auto">
+                <div className="flex items-center justify-between p-4">
+                    <div className="flex items-center">
+                        <a href="/">
+                            <Image src="/logo.png" alt="Logo" width={60} height={60}/>
+                        </a>
+                    </div>
+                    <div className="flex items-center gap-x-4">
+                        <a href="/about" className="text-2xl font-bold uppercase">
+                            About
+                        </a>
+                        <a href="/contact" className="text-2xl font-bold uppercase">
+                            Contact
+                        </a>
+                    </div>
+                    <div className="flex items-center gap-x-4">
+                        <a href="/account">
+                            <UserCircleIcon className="size-6"/>
+                        </a>
+                        <a href="/cart">
+                            <ShoppingCartIcon className="size-6"/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
