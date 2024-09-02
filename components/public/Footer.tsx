d98e1eb8e8895f9e925b723dc2a1a55bde595b27
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-02 16:21:59 -0700

    Improve footer style.

--- components/public/Footer.tsx
+++ components/public/Footer.tsx
@@ -6,32 +6,44 @@ const Footer = () => {
     return (
         <footer className="bg-black text-white">
             <div className="container">
-                <nav className="flex items-center justify-center flex-wrap px-4 py-8 gap-4">
-                    <Link href="/">
-                        <Image src="/logo/logo_white.png" alt="Logo" width={40} height={40}/>
-                    </Link>
+                <nav className="flex align-top justify-center flex-wrap px-4 py-8 gap-4">
+                    <div className="flex flex-col items-center mr-12 gap-y-3">
+                        <Link href="/">
+                            <Image src="/logo/logo_white.png" alt="Logo" width={40} height={40}/>
+                        </Link>
+                        <div className="italic text-sm text-center">
+                            Antoine Le Calloch {new Date().getFullYear()}<br/>
+                            All rights reserved &copy;
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-self-start items-center mx-8">
+                        <div className="font-bold mb-4">
+                            Information
+                        </div>
+                        <a href="/about" className="uppercase">
+                            About
+                        </a>
+                        <a href="/contact" className="uppercase">
+                            Contact
+                        </a>
+                    </div>
                     |
-                    <a href="/about" className="font-bold uppercase">
-                        About
-                    </a>
+                    <div className="flex flex-col justify-self-start items-center mx-8">
+                        <div className="font-bold mb-4">
+                            Policies
+                        </div>
+                        <a href="/privacy_policy" className="uppercase">
+                            Privacy Policy
+                        </a>
+                        <a href="/refund_policy" className="uppercase">
+                            Refund Policy
+                        </a>
+                        <a href="/shipping_policy" className="uppercase">
+                            Shipping Policy
+                        </a>
+                    </div>
                     |
-                    <a href="/contact" className="font-bold uppercase">
-                        Contact
-                    </a>
-                    |
-                    <a href="/privacy_policy" className="font-bold uppercase">
-                        Privacy Policy
-                    </a>
-                    |
-                    <a href="/refund_policy" className="font-bold uppercase">
-                        Refund Policy
-                    </a>
-                    |
-                    <a href="/shipping_policy" className="font-bold uppercase">
-                        Shipping Policy
-                    </a>
-                    |
-                    <a href="/terms_of_sale" className="font-bold uppercase">
+                    <a href="/terms_of_sale" className="font-bold mx-8">
                         Terms of Sale
                     </a>
                 </nav>
