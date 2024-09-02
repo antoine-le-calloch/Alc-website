9f8e181d6eec994d25a5563ffa840ab88095eabd
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-02 12:16:58 -0700

    Move component to public folder and layout and page to (public) group.

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,17 +1,16 @@
+import React from 'react';
 import type { Metadata } from "next";
+import { Header, Footer } from "@/components/public";
 import "./globals.css";
-import { Header, Footer } from "@/components";
 
 export const metadata: Metadata = {
   title: "Website Title",
   description: "This is a website description",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function PublicLayout({children}: {
+    children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className="relative min-h-screen flex flex-col">
---
+++ components/public/index.ts
@@ -0,0 +1,4 @@
+import Header from "@/components/public/Header";
+import Footer from "@/components/public/Footer";
+
+export { Header, Footer };
