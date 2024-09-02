60053a66a0443581a657112679cb2f0da590aece
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-01 22:28:26 -0700

    Implementation of the footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="bg-black text-white">
+            <div className="container">
+                <div className="flex items-center justify-center flex-wrap px-4 py-8 gap-4">
+                    <a href="/about" className="text-l font-bold uppercase">
+                        About
+                    </a>
+                    |
+                    <a href="/contact" className="text-l font-bold uppercase">
+                        Contact
+                    </a>
+                    |
+                    <a href="/privacy_policy" className="text-l font-bold uppercase">
+                        Privacy Policy
+                    </a>
+                    |
+                    <a href="/refund_policy" className="text-l font-bold uppercase">
+                        Refund Policy
+                    </a>
+                    |
+                    <a href="/shipping_policy" className="text-l font-bold uppercase">
+                        Shipping Policy
+                    </a>
+                    |
+                    <a href="/terms_of_sale" className="text-l font-bold uppercase">
+                        Terms of Sale
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
