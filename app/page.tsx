a7aa64f72309e2f56af6116fdaf43e5f5bb138a7
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-01 21:54:23 -0700

    Add container and title h2 and text to home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 export default function Home() {
   return (
-    <main className="flex-grow">
-        <div className="flex flex-col items-center justify-center">
-            <h1>Home</h1>
-        </div>
-    </main>
+      <main className="flex-grow">
+          <div className="container max-w-full">
+              <div className="flex items-center justify-center h-80 bg-slate-100 mb-4">
+                  <h1>Home</h1>
+              </div>
+          </div>
+          <div className="container py-8">
+              <div className="flex flex-col items-center justify-center mb-4">
+                  <h2>Title 1</h2>
+                  <div>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
+                      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+              </div>
+          </div>
+          <div className="container py-8">
+              <div className="flex flex-col items-center justify-center">
+                  <h2>Title 2</h2>
+                  <div>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
+                      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+              </div>
+          </div>
+      </main>
   );
 }
