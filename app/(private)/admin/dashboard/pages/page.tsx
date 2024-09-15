7e4233a8243ac9bf2d86ed46df18d9a5fea951ec
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-14 21:47:26 -0700

    Create a simple page for the pages management.

---
+++ app/(private)/admin/dashboard/pages/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+    return (
+        <div className="container p-4">
+            <h2>
+                Pages management
+            </h2>
+        </div>
+    )
+};
+
+export default DashboardPage;
