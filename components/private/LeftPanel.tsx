9ce325ab932caa418be56bb60b5c9cecb49516ac
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-14 21:54:31 -0700

    Add a margin on the left panel.

--- components/private/LeftPanel.tsx
+++ components/private/LeftPanel.tsx
@@ -7,7 +7,7 @@ const LeftPanel = () => {
     return (
         <div className="flex flex-col justify-between items-center h-screen p-4 panel-color shadow-xl">
             <Image src="/logo/logo.svg" alt="Logo" width={40} height={40}/>
-            <div className="flex flex-col gap-4 text-center text-xl">
+            <div className="flex flex-col gap-4 text-center text-xl mx-2">
                 <Link href="dashboard/pages">
                     Pages
                 </Link>
