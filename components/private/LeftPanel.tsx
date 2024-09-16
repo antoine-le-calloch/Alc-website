e78b03bf8aaf070067505551e0ecac62bc8bb8c7
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-16 15:37:46 +0200

    Add Link to left panel text.

--- components/private/LeftPanel.tsx
+++ components/private/LeftPanel.tsx
@@ -6,10 +6,12 @@ import {Cog6ToothIcon} from "@heroicons/react/24/outline";
 const LeftPanel = () => {
     return (
         <div className="flex flex-col justify-between items-center h-screen p-4 panel-color shadow-xl">
-            <Image src="/logo/logo.svg" alt="Logo" width={40} height={40}/>
+            <Link href="/admin/dashboard">
+                <Image src="/logo/logo.svg" alt="Logo" width={40} height={40}/>
+            </Link>
             <div className="flex flex-col gap-4 text-center text-xl mx-2">
-                <Link href="dashboard/pages">
-                    Pages
+                <Link href="/admin/dashboard/pages">
+                    PAGES
                 </Link>
             </div>
             <div>
