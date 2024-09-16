11838b879fb6fec222f73ea7ac92c11fd1617b4a
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-16 15:38:43 +0200

    Update dashboard page to display topBar and change the padding from container to text div.

--- app/(private)/admin/dashboard/page.tsx
+++ app/(private)/admin/dashboard/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import React from 'react';
+import {TopBar} from "@components/private";
 
 const DashboardPage: React.FC = () => {
     return (
-        <div className="container p-4">
-            <div>
-                Content loading...
+        <div>
+            <TopBar titleName={"Dashboard"}/>
+            <div className="container">
+                <div className="p-4">
+                    Content loading...
+                </div>
             </div>
         </div>
     )
