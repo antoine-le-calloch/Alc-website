c6f41fd08a2b76d6060f76fdcfe81f9cae926021
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-17 17:38:14 +0200

    Improve the creation of a new page by using title and clean the field after each creation.

--- app/(private)/admin/dashboard/pages/new/page.tsx
+++ app/(private)/admin/dashboard/pages/new/page.tsx
@@ -1,47 +1,51 @@
 "use client";
 
-import React from 'react';
-
-type Page = {
-    title: string;
-    text: string;
-};
+import React, {useState} from 'react';
+import Button from "@components/private/Button";
 
 const PagesPage = () => {
+    const [title, setTitle] = useState("")
     
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+    const handleSubmit = async () => {
         const response = await fetch('/api/pages', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
-            }
+            },
+            body: JSON.stringify({
+                title: title
+            })
         });
         if (response.ok) {
+            setTitle("");
             alert("New page successfully created !");
+        }else{
+            console.log(response)
         }
     }
     
     return (
         <div>
-            <div className="flex justify-between items-center py-2">
+            <div className="flex justify-between items-center py-2 mb-4">
                 <h2>
                     New page creation
                 </h2>
             </div>
-            <form>
-                <div className="flex flex-col">
-                    <label>Title</label>
-                    <input type="text" />
+            <div>
+                <div className="flex flex-col mb-4">
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Title"
+                        className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm 
+                        focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+                    />
                 </div>
-                <div className="flex flex-col">
-                    <label>Text</label>
-                    <textarea />
-                </div>
-                <button type="submit" onClick={handleSubmit}>
+                <Button onClick={handleSubmit} type="submit">
                     Create
-                </button>
-            </form>
+                </Button>
+            </div>
         </div>
     )
 };
