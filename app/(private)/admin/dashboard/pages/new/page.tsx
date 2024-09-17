e8c08577a5779c098787f9551669c44be4ca0cf1
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-17 16:35:51 +0200

    Add a simple page to create a page.

---
+++ app/(private)/admin/dashboard/pages/new/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from 'react';
+
+type Page = {
+    title: string;
+    text: string;
+};
+
+const PagesPage = () => {
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const response = await fetch('/api/pages', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+    
+    return (
+        <div>
+            <div className="flex justify-between items-center py-2">
+                <h2>
+                    New page creation
+                </h2>
+            </div>
+            <form>
+                <div className="flex flex-col">
+                    <label>Title</label>
+                    <input type="text" />
+                </div>
+                <div className="flex flex-col">
+                    <label>Text</label>
+                    <textarea />
+                </div>
+                <button type="submit" onClick={handleSubmit}>
+                    Create
+                </button>
+            </form>
+        </div>
+    )
+};
+
+export default PagesPage;
