2a6ad78b012ff65d3f0b5a48eb09e6e7a79dc3e8
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-09-17 14:17:09 +0200

    Improve the page list style and delete the post.

--- app/(private)/admin/dashboard/pages/page.tsx
+++ app/(private)/admin/dashboard/pages/page.tsx
@@ -1,57 +1,54 @@
 "use client";
 
 import React, {useEffect, useState} from 'react';
-import {PlusIcon} from "@heroicons/react/24/solid";
-import {collection, addDoc, getDocs} from 'firebase/firestore';
-import { db } from '@lib/firebase';
 import {TopBar} from "@components/private";
+import Link from "next/link";
 
-const PagesPage: React.FC = () => {
-    const [pages, setPages] = useState<any[]>([]);
+type Page = {
+    title: string;
+    text: string;
+};
 
-    const fetchPages = async () => {
-        try {
-            const querySnapshot = await getDocs(collection(db, 'pages'));
-            const pagesList = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-            setPages(pagesList);
-        } catch (e) {
-            alert('Error fetching pages: ' + e);
-        }
-    };
+const PagesPage = () => {
+    const [pages, setPages] = useState([])
+    const [message, setMessage] = useState('Loading...')
 
     useEffect(() => {
-        fetchPages();
-    }, []);
-    
-    const handleSubmit = async () => {
-        try {
-            await addDoc(collection(db, 'pages'), {
-                title: "title",
-                text: "text",
-            });
-        } catch (e) {
-            alert('Error creating page: ' + e);
+        async function fetchPosts() {
+            let res = await fetch('/api/pages')
+            let data = await res.json()
+            setPages(data)
+            if(data.length == 0)
+                setMessage('No pages already create')
         }
-    };
-    
+        fetchPosts()
+    }, [])
+
     return (
         <div>
             <TopBar titleName={"Pages management"}/>
             <div className="container p-4">
-                <div>
-                    Create a new page
+                <div className="flex justify-between items-center py-2">
+                    <h2>
+                        List
+                    </h2>
+                    <Link href="/admin/dashboard/pages/new" 
+                          className="rounded bg-blue-300 hover:bg-blue-400 py-1 px-2">
+                        New
+                    </Link>
                 </div>
-                <div className="m-5">
-                    <button type="submit" onClick={handleSubmit}>
-                        <PlusIcon className="size-8 rounded bg-blue-300 hover:bg-blue-400"/>
-                    </button>
+                <div className="flex justify-center items-center border-t border-gray-500">
+                    { pages.length === 0 ? (
+                        <div className="my-4">{message}</div>
+                    ) : (
+                        pages.map((post: Page, index: number) => (
+                            <div key={index} className="border-b border-gray-200 p-4">
+                                <div className="font-bold">{post.title}</div>
+                                <div>{post.text}</div>
+                            </div>
+                        ))
+                    )}
                 </div>
-                {pages.map((page) => (
-                    <div key={page.id} className="flex gap-8 border-b-2 border-gray-200 p-2">
-                        <div>{page.title}</div>
-                        <div>{page.text}</div>
-                    </div>
-                ))}
             </div>
         </div>
     )
