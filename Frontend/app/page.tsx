e8d514aaaca913ed02d0a0662c915382f82fc4f4
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-10-31 13:43:24 +0100

    Improve frontend Home page.

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,29 +1,11 @@
 export default function Home() {
-  return (
-      <main className="flex-grow">
-          <div className="container max-w-full">
-              <div className="flex items-center justify-center h-80 bg-slate-100 mb-4">
-                  <h1>Home</h1>
-              </div>
-          </div>
-          <div className="container py-8">
-              <div className="flex flex-col items-center justify-center mb-4">
-                  <h2>Title 1</h2>
-                  <div className="text-center">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
-                      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  </div>
-              </div>
-          </div>
-          <div className="container py-8">
-              <div className="flex flex-col items-center justify-center">
-                  <h2>Title 2</h2>
-                  <div className="text-center">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
-                      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  </div>
-              </div>
-          </div>
-      </main>
-  );
+    return (
+        <main className="flex-grow">
+            <div className="container">
+                <div className="text-center py-8">
+                    <h1>Home</h1>
+                </div>
+            </div>
+        </main>
+    );
 }
